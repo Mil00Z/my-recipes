5cccbf8745d5c8375fc7a49880876b16ceba9cd2
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-05-28 17:12:31 +0200

    Init Project : quick add Alias + next config params

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true, // Active une vérification plus stricte (optionnel)
+  compiler: {
+    styledComponents: true, // Peut corriger certains problèmes liés aux entités dans les styles
+  },
 };
 
 export default nextConfig;
