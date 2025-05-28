a4e1749870638f5b6bf0999d8096d9fd70b00131
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-05-28 18:57:01 +0200

    Page : HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,103 @@
+'use client';
+
+import {useState} from "react";
+
+import Link from "next/link";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+const Home = () => {
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+const [countRecipes, setCountRecipes] = useState<number>(10);
+
+return(
+<>
+  <header className="main-header">
+
+    <div className="top-header">
+        <Link href="/" aria-label={"lien vers la Homepage"} tabIndex={0}>
+          <Image src="/logo.svg" alt="logo de My-Recipes" width={180} height={38} priority title="V2" />
+        </Link>
+        <Link href="/starter" aria-label={"lien vers le starter"} className="link">
+            Starter
+        </Link>
+    </div> 
+
+    <div className="hero">
+        <h1 className="hero-title">cherchez parmi plus de <span className="initial-count">{countRecipes}</span> recettes<br/>
+            du quotidien,simples et délicieuses
+        </h1>
+
+        <form action="/" className="the-form">
+            <label htmlFor="main-search" className="v-hidden">Recherche</label>
+            <input type="search" name="main-search" id="main-search" placeholder="Rechercher une recette, un ingrédient..."/>
+            <button type="submit" className="btn btn-search" aria-label="bouton de recherche"><i className="fa-solid fa-search"></i></button>
+        </form>
     </div>
-  );
+
+   </header>
+
+   <main className="wrapper">
+
+    <section className="recipes-filter">
+
+        <form action="/" className="form-select">
+
+            <div className="filters-group">
+
+                <div className="filters">
+                    <label htmlFor="ingredients" className="labels">Ingrédients <i className="fa-solid fa-angle-down"></i></label>
+                    <input type="search" name="ingredients" id="ingredients" className="search-filter"/>
+
+                    <ul className="search-results">
+                    </ul>
+                </div>
+
+                <div className="filters">
+                    <label htmlFor="appliances" className="labels">Appareils<i className="fa-solid fa-angle-down"></i></label>
+                    <input type="search" name="appliances" id="appliances" className="search-filter"/>
+
+                    <ul className="search-results">
+                    </ul>
+                </div>
+
+                <div className="filters">
+                    <label htmlFor="ustensils" className="labels">Ustensiles<i className="fa-solid fa-angle-down"></i></label>
+                    <input type="search" name="ustensils" id="ustensils" className="search-filter"/>
+
+                    <ul className="search-results">
+                    </ul>
+                </div>
+
+                <div className="filters">
+                    <label htmlFor="timing" className="labels">Minutages<i className="fa-solid fa-angle-down"></i></label>
+                    <input type="search" name="timing" id="timing" className="search-filter"/>
+
+                    <ul className="search-results">
+                    </ul>
+                </div>
+
+            </div>
+            
+            <div className="recipe-taglist">
+
+            </div>
+
+        </form>
+
+        <div className="recipes-counter" aria-label="nombre de recettes disponibles"> <span className="count">{countRecipes}</span> recette(s)</div>
+
+    </section>
+
+    <section className="recipes-container">
+    </section>
+
+   </main>
+<div className="debeug"></div>
+
+    <footer className="main-footer">
+      <div className="copyright">&copy; 2024 - Les Petits Plats by <a href="https://github.com/Mil00Z"><span> Mil00Z </span></a></div>
+    </footer>
+  </>
+  )
 }
+export default Home;
