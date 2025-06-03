5ac4ff9246345158600b5e8aca13b8094b04a6c5
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-03 16:46:42 +0200

    Ui : SearchForm component + get datas from props

---
+++ src/components/SearchForm/SearchForm.tsx
@@ -0,0 +1,33 @@
+
+import { useStore } from "@/hooks/dataStore";
+import type { Recipe } from "@/types/recipe.types";
+
+import "./SearchForm.scss";
+
+
+interface SearchFormProps {
+  initialRecipes: Recipe[];
+  matchingRecipes: Recipe[];
+  count:number;
+} 
+
+const SearchForm = ({initialRecipes,matchingRecipes,count}:SearchFormProps) => {
+
+
+//Checking Datas
+function TestingInput(element) {
+
+  console.log(element);
+
+}
+
+return(
+
+    <form action="/" className="the-form">
+        <label htmlFor="main-search" className="v-hidden">Recherche</label>
+        <input type="search" name="main-search" id="main-search" onChange={(event) => TestingInput(event.target.value)} placeholder="Rechercher une recette, un ingrédient..."/>
+        <button type="submit" className="btn btn-search" aria-label="bouton de recherche"><i className="fa-solid fa-search"></i>{count}</button>
+    </form>
+  )
+}
+export default SearchForm
