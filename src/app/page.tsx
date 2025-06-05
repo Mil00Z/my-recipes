f72ac6efd10c021264c1d786c780d20efb1f7fe9
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-05 17:11:59 +0200

    UI : Import Filter type + Component Filter + Create datas to create FIlters List

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,84 +3,85 @@
 import {useEffect} from "react";
 
 import { useStore } from "@/hooks/dataStore";
-
+import type { Filter } from "@/types/filter.types";
 
 
 //UI
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
+import FilterSearch from "@/components/Filters/Filter";
 import RecipesList from "@/components/RecipesList/RecipesList";
+
+
+const FiltersDatas : Filter[] = [
+
+  {
+    type : 'ingredients',
+    title: 'Ingrédients',
+    method : (value:string) => console.log(value)
+  },
+  {
+    type : 'appliances',
+    title: 'Appareils',
+    method : (value:string) => console.log(value)
+  },
+  {
+    type : 'ustensils',
+    title: 'Ustensiles',
+    method : (value:string) => console.log(value)
+  },
+  {
+    type : 'timing',
+    title: 'Minutages',
+    method : (value:string) => console.log(value)
+  }
+]
+
 
 
 const Home = () => {
 
 const {recipes,count,matchingRecipes,incrementCount} = useStore();
 
+// useEffect(() => {
 
-useEffect(() => {
-
-    console.log(recipes,count,matchingRecipes);
+//     console.log(recipes,count,matchingRecipes);
     
-}, [count]);
-
+// }, [count]);
 
 
 return(
  
-        <PageWrapper layout="home" >
+    <PageWrapper layout="home" >
 
-            <section className="recipes-filter debeug">
-  <form action="/" className="form-select">
-    <div className="filters-group">
-      <div className="filters">
-        <label htmlFor="ingredients" className="labels">
-          Ingrédients <i className="fa-solid fa-angle-down"></i>
-        </label>
-        <input type="search" name="ingredients" id="ingredients" className="search-filter"/>
-        <ul className="search-results"></ul>
-      </div>
+      <section className="recipes-filter">
+          <form action="/" className="form-select">
+            <div className="filters-group">
 
-      <div className="filters">
-        <label htmlFor="appliances" className="labels">
-          Appareils<i className="fa-solid fa-angle-down"></i>
-        </label>
-        <input type="search" name="appliances" id="appliances" className="search-filter"/>
-        <ul className="search-results"></ul>
-      </div>
+              {FiltersDatas.map((filter:Filter) => {
 
-      <div className="filters">
-        <label htmlFor="ustensils" className="labels">
-          Ustensiles<i className="fa-solid fa-angle-down"></i>
-        </label>
-        <input type="search" name="ustensils" id="ustensils" className="search-filter"/>
-        <ul className="search-results"></ul>
-      </div>
+                  return(<FilterSearch key={filter.type} type={filter.type} title={filter.title} method={filter.method} />)
+              })}
 
-      <div className="filters">
-        <label htmlFor="timing" className="labels">
-          Minutages<i className="fa-solid fa-angle-down"></i>
-        </label>
-        <input type="search" name="timing" id="timing" className="search-filter"/>
-        <ul className="search-results"></ul>
-      </div>
-    </div>
+            </div>
                 
-    <div className="recipe-taglist"></div>
-  </form>
+            <div className="recipe-taglist"></div>
 
-  <div className="recipes-counter" aria-label="nombre de recettes disponibles">
-    <span className="count">{count}</span> recette(s)
-  </div>
+            </form>
 
-  <button className="btn btn-cta" onClick={() => incrementCount()}>
-    one up +1
-  </button>
-            </section>
+            <div className="recipes-counter" aria-label="nombre de recettes disponibles">
+              <span className="count">{count}</span> recette(s)
+            </div>
 
-          <section className="recipes-container debeug">
-            <RecipesList recipes={recipes} matchingRecipes={matchingRecipes} />
-            </section>
+            <button className="btn btn-cta" onClick={() => incrementCount()}>
+              one up +1
+            </button>
+      </section>
 
-        </PageWrapper>
+      <section className="recipes-container">
+          <RecipesList recipes={recipes} matchingRecipes={matchingRecipes} />
+      </section>
+
+    </PageWrapper>
 
 )
 }
