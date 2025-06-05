7b0db02c7b6e1b2240fdde29b4e2592cba4773cd
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-05 12:54:30 +0200

    UI: PageWrapper Integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,86 @@
 
 import {useEffect} from "react";
 
-import Link from "next/link";
-import Image from "next/image";
-
-
-import Hero from "@/components/Hero/Hero";
-
-
 import { useStore } from "@/hooks/dataStore";
+
+
+
+//UI
+import PageWrapper from "@/components/PageWrapper/PageWrapper";
+import RecipesList from "@/components/RecipesList/RecipesList";
+
 
 const Home = () => {
 
-const {initialRecipes,count,matchingRecipes,incrementCount} = useStore();
-
-console.log(initialRecipes,count,matchingRecipes);
-
-
+const {recipes,count,matchingRecipes,incrementCount} = useStore();
 
 
 useEffect(() => {
 
-
+    console.log(recipes,count,matchingRecipes);
+    
 }, [count]);
 
 
 
 return(
-<>
-  <header className="main-header">
+ 
+        <PageWrapper layout="home" >
 
-    <div className="top-header">
-        <Link href="/" aria-label={"lien vers la Homepage"} tabIndex={0}>
-          <Image src="/logo.svg" alt="logo de My-Recipes" width={180} height={38} priority title="V2" />
-        </Link>
-        <Link href="/starter" aria-label={"lien vers le starter"} className="link">
-            Starter
-        </Link>
-    </div> 
+            <section className="recipes-filter debeug">
+  <form action="/" className="form-select">
+    <div className="filters-group">
+      <div className="filters">
+        <label htmlFor="ingredients" className="labels">
+          Ingrédients <i className="fa-solid fa-angle-down"></i>
+        </label>
+        <input type="search" name="ingredients" id="ingredients" className="search-filter"/>
+        <ul className="search-results"></ul>
+      </div>
 
-    <Hero />
+      <div className="filters">
+        <label htmlFor="appliances" className="labels">
+          Appareils<i className="fa-solid fa-angle-down"></i>
+        </label>
+        <input type="search" name="appliances" id="appliances" className="search-filter"/>
+        <ul className="search-results"></ul>
+      </div>
 
-   </header>
+      <div className="filters">
+        <label htmlFor="ustensils" className="labels">
+          Ustensiles<i className="fa-solid fa-angle-down"></i>
+        </label>
+        <input type="search" name="ustensils" id="ustensils" className="search-filter"/>
+        <ul className="search-results"></ul>
+      </div>
 
-   <main className="wrapper">
+      <div className="filters">
+        <label htmlFor="timing" className="labels">
+          Minutages<i className="fa-solid fa-angle-down"></i>
+        </label>
+        <input type="search" name="timing" id="timing" className="search-filter"/>
+        <ul className="search-results"></ul>
+      </div>
+    </div>
+                
+    <div className="recipe-taglist"></div>
+  </form>
 
-    <section className="recipes-filter">
+  <div className="recipes-counter" aria-label="nombre de recettes disponibles">
+    <span className="count">{count}</span> recette(s)
+  </div>
 
-        <form action="/" className="form-select">
+  <button className="btn btn-cta" onClick={() => incrementCount()}>
+    one up +1
+  </button>
+            </section>
 
-            <div className="filters-group">
+          <section className="recipes-container debeug">
+            <RecipesList recipes={recipes} matchingRecipes={matchingRecipes} />
+            </section>
 
-                <div className="filters">
-                    <label htmlFor="ingredients" className="labels">Ingrédients <i className="fa-solid fa-angle-down"></i></label>
-                    <input type="search" name="ingredients" id="ingredients" className="search-filter"/>
+        </PageWrapper>
 
-                    <ul className="search-results">
-                    </ul>
-                </div>
-
-                <div className="filters">
-                    <label htmlFor="appliances" className="labels">Appareils<i className="fa-solid fa-angle-down"></i></label>
-                    <input type="search" name="appliances" id="appliances" className="search-filter"/>
-
-                    <ul className="search-results">
-                    </ul>
-                </div>
-
-                <div className="filters">
-                    <label htmlFor="ustensils" className="labels">Ustensiles<i className="fa-solid fa-angle-down"></i></label>
-                    <input type="search" name="ustensils" id="ustensils" className="search-filter"/>
-
-                    <ul className="search-results">
-                    </ul>
-                </div>
-
-                <div className="filters">
-                    <label htmlFor="timing" className="labels">Minutages<i className="fa-solid fa-angle-down"></i></label>
-                    <input type="search" name="timing" id="timing" className="search-filter"/>
-
-                    <ul className="search-results">
-                    </ul>
-                </div>
-
-            </div>
-            
-            <div className="recipe-taglist">
-
-            </div>
-
-        </form>
-
-        <div className="recipes-counter" aria-label="nombre de recettes disponibles"> <span className="count">{count}</span> recette(s)</div>
-
-        <button className="btn btn-cta" onClick={() => incrementCount()}>one up +1</button>
-
-    </section>
-
-    <section className="recipes-container">
-    </section>
-
-   </main>
-
-    <footer className="main-footer">
-      <div className="copyright">&copy; {new Date().getFullYear()} - My Recipes by <a href="https://github.com/Mil00Z/my-recipes"><span> Mil00Z </span></a></div>
-    </footer>
-
-  </>
-  )
+)
 }
 export default Home;
---
+++ src/app/recipe/[id]/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import {Image} from "next/image";
+import { useStore } from "@/hooks/dataStore";
+import type { Recipe } from "@/types/recipe.types";
+import { useParams } from "next/navigation";
+
+
+import PageWrapper from "@/components/PageWrapper/PageWrapper";
+
+
+import "./Recipe.scss";
+
+
+const Recipe = () => {
+
+  const {recipes} = useStore();
+
+  const getParams = useParams();
+
+  const recipe = recipes.find((recipe:Recipe) => {
+    // console.log('recipe', typeof recipe.id);
+    // console.log('getParams', typeof getParams.id);
+    return recipe.id === getParams.id}
+  );
+
+
+ if (!recipes.length) {
+    return (
+      <div className="debeug recipe-loading">Loading Recipes...</div>
+    )
+  }
+
+  if (!recipe) {
+
+    return (
+      <div className="debeug recipe-not-found">Recipe not found</div>
+    )
+  }
+
+
+  return (
+  <>
+      <PageWrapper>
+
+        <div className="recipe-single">
+          <h1>{recipe.title}</h1>
+          {/* <Image
+            src={recipe.image}
+            alt={recipe.title}
+            width={800}
+            height={600}
+            className="recipe-cover"
+          /> */}
+          <img src={recipe.image} alt={recipe.title} />
+          <p>{recipe.description}</p>
+          <p>Timing : {recipe.time} min(s)</p>
+          <p>Pour : {recipe.servings} gourmand(es)</p>
+          <p>Appareil principal : {recipe.appliance}</p>
+          <p>Ustensiles : {recipe.ustensils.join(' - ')}</p>
+        </div>
+
+      </PageWrapper>
+  </>
+  )
+}
+export default Recipe
