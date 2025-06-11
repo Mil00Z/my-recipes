707f0865bcef4910d04079178a666ba99c7ffd09
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-11 18:06:07 +0200

    Tag : Add to Store + Create Actions

--- src/hooks/dataStore.tsx
+++ src/hooks/dataStore.tsx
@@ -1,7 +1,10 @@
 import { create } from 'zustand';
-import {persist} from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 
 import type { Recipe } from '@/types/recipe.types';
+import type { Tag	 } from '@/types/tag.types';
+
+//Datas
 import {recipes as initialRecipes} from '@/datas/recipes';
 
 
@@ -9,27 +12,36 @@ type Store = {
   recipes: Recipe[];
   count:number;
   matchingRecipes: Recipe[];
+  tags: Tag[];
   incrementCount: () => void;
   updateResults: (results: Recipe[]) => void;
   resetResults: () => void;
+  updateTags: (tag:Tag[]) => void;
 };  
 
 export const useStore = create<Store>(
   persist((set) => ({
-  recipes: initialRecipes,
-  count:initialRecipes.length,
-  matchingRecipes: [],
-  incrementCount: () => set((state) => ({ count: state.count + 1})),
-  updateResults: (results:Recipe[]) => set((state) => ({    matchingRecipes:results,
-  count:results?.length ?? state.initialRecipes.length
-  })),
-  resetResults: () => set((state) => ({ 
-  matchingRecipes: [],
-  count: initialRecipes.length
-  })),
-}),{
-  name:'recipes-stored',
-  }
-)
+    recipes: initialRecipes,
+    count:initialRecipes.length,
+    matchingRecipes: [],
+    tags: [],
+    incrementCount: () => set((state) => ({ count: state.count + 1})),
+    updateResults: (results:Recipe[]) => set((state) => ({    matchingRecipes:results,
+    count:results?.length ?? state.initialRecipes.length
+    })),
+    resetResults: () => set((state) => ({ 
+    matchingRecipes: [],
+    count: initialRecipes.length
+    })),
+    updateTags: (tag:Tag) => set((state) => ({
+      tags:[...state.tags,tag]
+    })),
+    removeTag : (tag:Tag) => set((state) => ({
+      tags:[]
+    }))
+  }),{
+    name:'recipes-stored',
+    }
+  )
 );
 
