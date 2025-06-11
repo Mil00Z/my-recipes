b90ca514c4fee0cb489eaa95d145dbb3638f51ec
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-11 18:06:45 +0200

    Debug : Add Tag Datas Store in store Panel

--- src/components/Debeug/Debeug.tsx
+++ src/components/Debeug/Debeug.tsx
@@ -2,15 +2,25 @@ import { useStore } from "@/hooks/dataStore";
 
 const StoreDebbuger = () => {
 
- const {matchingRecipes,count} = useStore();
+ const {matchingRecipes,tags} = useStore();
 
 return(
    <div className="store-debbuger">
+
+       <h3>Store Debug - tags: <span>{tags.length}</span></h3>
+      <pre>
+        {JSON.stringify({ tags }, null, 1)}
+      </pre>
+
+
       <h3>Store Debug - Count: <span>{matchingRecipes.length}</span></h3>
   
       <pre>
         {JSON.stringify({ matchingRecipes }, null, 1)}
       </pre>
+
+
+     
       
     </div>
 )
