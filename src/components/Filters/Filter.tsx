c5790a9d75cb99a9cc49baf935a10950f669c829
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-11 12:27:52 +0200

    Filter : Create logic function with switch + pré variable before render (separate logic / ui) + Styles

--- src/components/Filters/Filter.tsx
+++ src/components/Filters/Filter.tsx
@@ -1,11 +1,53 @@
 
 import type { Filter } from "@/types/filter.types";
+import type { Recipe } from "@/types/recipe.types";
+
+import { useStore } from "@/hooks/dataStore";
 
 //Styles
 import "./Filter.scss";
 
 
 const FilterSearch = ({type,title,method}:Filter) => {
+
+    const {matchingRecipes} = useStore();
+
+    const getFilterDatas = () => {
+
+        let filtersDatas: any[] = [];    
+
+    switch(type) {
+    
+        case 'appliances':
+        let allAppliances = matchingRecipes.map((recipe:Recipe) => recipe.appliance.toLowerCase());
+       
+        // /Get distinct item of a collection of values
+        filtersDatas = Array.from(new Set(allAppliances)).sort((a,b) => {
+            return a.localeCompare(b);
+        });
+
+        return filtersDatas;
+
+        case 'timing':
+        let allTiming = matchingRecipes.map((recipe:Recipe) => recipe.time);
+       
+        // /Get distinct item of a collection of values
+       filtersDatas = Array.from(new Set(allTiming)).sort((a,b) => {
+            return a - b;
+        });
+
+        return filtersDatas;
+
+
+        default:
+        return [];
+    }
+
+};
+
+    const filters = getFilterDatas();
+
+    console.log('getFilterDatas', filters);
 
   return (
       <>
@@ -14,7 +56,11 @@ const FilterSearch = ({type,title,method}:Filter) => {
                 {title} <i className="fa-solid fa-angle-down"></i>
             </label>
             <input type="search" name={type} id={type} className="search-filter" onChange={(event) => method(event.target.value)}/>
-            <ul className="search-results"></ul>
+            <ul className="search-results">
+                {filters.map((element) => {
+                    return <li key={element} className="option">{element}</li>;
+                })}
+            </ul>
         </div>
     </>
 )
