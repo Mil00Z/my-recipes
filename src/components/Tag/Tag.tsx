5103cab559ec15e4c7843f9221ab7f96d9ff235d
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-12 18:56:11 +0200

    Tag :  change handler function to remove

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -11,18 +11,24 @@ const TagElement = ({element} : TagProps) => {
 
  const {tags,removeTag} = useStore();
 
+
+  function handleRemoveTag (tag:Tag)  {
+
+    removeTag(tag)
+
+ }
+
+
   return (
-    
     <>
-      {tags?.map((tag:Tag) => {
+      {tags?.map((tag:Tag,index:number) => {
         return(
-          <span key={tag.value} className="recipe-tag" data-parent={tag.type}>{tag.value}
-            <i className="fa-solid fa-xmark" onClick={() => removeTag(tag.value,tag.type)}></i>
+          <span key={`${tag.type}-${index}`} className="recipe-tag" data-parent={tag.type}>{tag.value}
+            <i className="fa-solid fa-xmark" onClick={() => handleRemoveTag(tag)}></i>
           </span>
         )
       })}
     </>
-  
   )
 }
 export default TagElement
