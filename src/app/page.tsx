43c076029068df3d90cdb16e541aa883a0965374
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-06-20 18:14:59 +0200

    Home : Add reset Tag Component + Testing update Datas from deleting tag from Parent Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 import { useStore } from "@/hooks/dataStore";
 import type { Filter } from "@/types/filter.types";
+import type { Recipe } from "@/types/recipe.types";
+import type { Tag } from "@/types/tag.types";
 
 
 //UI
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
 import FilterSearch from "@/components/Filters/Filter";
+import ResetTag from "@/components/ResetTag/ResetTag";
 import RecipesList from "@/components/RecipesList/RecipesList";
 import TagElement from "@/components/Tag/Tag";
 import Counter from "@/components/Counter/Counter";
@@ -48,15 +51,53 @@ const Home = () => {
 
   useEffect(() => {
 
-    // if(tags.length === 0){
-    //   updateResults(recipes);
-    // }
-
     updateResults(recipes);
+
   }, []);
 
-  // console.log('nbre recettes initiales',count);
-  // console.log('nbre recettes updates',matchingRecipes.length);
+
+
+  useEffect(() => {
+    
+      // gros soucis de source de données, on tourne en rond
+      const baseSource = matchingRecipes?.length > 0 && matchingRecipes!== recipes ? matchingRecipes : recipes;
+
+  
+    if (tags.length === 0) {
+
+      updateResults(recipes);
+
+      return;
+    }
+
+    //Algo de Filtrage
+    const filteredResults = baseSource.filter((recipe:Recipe) =>
+      tags.every((tag:Tag) => {
+        switch (tag.type) {
+          case 'ingredients':
+            return recipe.ingredients.some(ing =>
+              ing.ingredient.toLowerCase() === tag.value.toLowerCase()
+            );
+          case 'ustensils':
+            return recipe.ustensils
+              .map(u => u.toLowerCase())
+              .includes(tag.value.toLowerCase());
+          case 'appliances':
+            return recipe.appliance.toLowerCase() === tag.value.toLowerCase();
+          case 'timing':
+            return recipe.time === parseInt(tag.value);
+          default:
+            return false;
+        }
+      })
+    );
+
+    
+    updateResults(filteredResults);
+    
+  }, [tags]);
+
+ 
 
 return(
  
@@ -70,6 +111,8 @@ return(
 
                   return(<FilterSearch key={filter.type} type={filter.type} title={filter.title} method={filter.method} />)
               })}
+
+              <ResetTag />
 
             </div>
                 
