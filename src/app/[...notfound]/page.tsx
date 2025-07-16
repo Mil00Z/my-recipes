0996e54b7c0f1710bcdbb5abdc7dffe39f9d5de4
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-07-16 11:17:13 +0200

    Build Errors : Escape Entities

--- src/app/[...notfound]/page.tsx
+++ src/app/[...notfound]/page.tsx
@@ -10,8 +10,10 @@ const PageNotFound = () => {
 
       <section className="not-found-container">
         <h1 className="title">404</h1>
-        <p className="description">Oups! La page que vous demandez n'existe pas.</p>
-        <Link href="/" className="link btn btn-cta">Retourner sur la page d'accueil</Link>
+        <p className="description">
+          {`Oups! La page que vous demandez n'existe pas.`}
+          </p>
+        <Link href="/" className="link btn btn-cta">{`Retourner sur la page d'accueil`}</Link>
       </section>  )
 
   }
