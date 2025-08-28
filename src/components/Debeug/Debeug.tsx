be7d760f97849c578a87a6e445509bc2aa988198
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-08-28 17:20:16 +0200

    Store Debeug : show State newRecipes in debeug

--- src/components/Debeug/Debeug.tsx
+++ src/components/Debeug/Debeug.tsx
@@ -5,19 +5,22 @@ import "./Debeug.scss"
 
 const StoreDebbuger = () => {
 
- const {matchingRecipes,tags} = useStore();
+ const {matchingRecipes,tags,newRecipes} = useStore();
 
 return(
    <div className="store-debbuger">
+
+    <h3>Store - Added New Recipes: <span>{newRecipes.length}</span></ h3>
+      <pre>
+        {JSON.stringify({ newRecipes }, null, 1)}
+      </pre>
 
        <h3>Store - Activ tags: <span>{tags.length}</span></h3>
       <pre>
         {JSON.stringify({ tags }, null, 1)}
       </pre>
 
-
       <h3>Store - Active Recipes: <span>{matchingRecipes.length}</span></h3>
-  
       <pre>
         {JSON.stringify({ matchingRecipes }, null, 1)}
       </pre>
