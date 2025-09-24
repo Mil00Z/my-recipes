5a7a7eb1267893e8dd4fa9ad98c5fbfbe88aaf5a
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-09-24 17:13:57 +0200

    Routing : Get Appliance only

---
+++ src/app/api/appliance/route.ts
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server';
+import { createClient } from '@supabase/supabase-js';
+
+// Récupérer les variables d'environnement.
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+// Vérifier que les variables d'environnement sont bien définies
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Les variables d\'environnement Supabase sont manquantes.');
+}
+
+// Créer le client Supabase avec la bonne clé de service
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Définir le gestionnaire de requête pour la méthode GET
+export async function GET(request : Request) {
+
+  try {
+    // Récupérer toutes les recettes depuis la table 'recipes'
+    const { data : appliances, error } = await supabase.from('Appliance').select('*');
+
+    // Si Supabase renvoie une erreur, retourner une réponse avec un message clair
+    if (error) {
+      console.error("❌ Erreur de connexion ou de requête Supabase:", error);
+      
+      return NextResponse.json(
+        { error: 'Erreur lors de la récupération des données..#1' }
+      );
+    }
+   
+    // Si la requête réussit, retournez les données sous forme de JSON
+    console.log("Données récupérées avec succès :", appliances);
+
+    return NextResponse.json(appliances);
+
+  } catch (err) {
+    // Gérer les erreurs inattendues, comme un problème de connexion
+    console.error('Erreur inattendue dans la route API:', err);
+    return NextResponse.json(
+      { error: 'Une erreur serveur est survenue...#2' },
+      { status: 500 }
+    );
+  }
+}
