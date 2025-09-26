764cc9eaaad5aa4d85feb094fa6b5cb6ecbd921d
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-09-26 11:40:55 +0200

    Testing Page for the API calls + debeug datas issues

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import {useState,useEffect} from 'react';
+import RecipeCard from '@/components/RecipeCard/RecipeCard';
+
+
+import type { Recipe } from '@/types/recipe.types';
+import PageWrapper from "@/components/PageWrapper/PageWrapper";
+import StoreDebbuger from "@/components/Debeug/Debeug";
+
+
+import '@/components/RecipeCard/RecipeCard.scss';
+
+
+const SandBoxPage = () => {
+
+  const [fetchedDatas,setFetchedDatas] = useState<Recipe[]>([])
+
+
+  const getDatas = async () => {
+
+
+        try {
+          
+          const response = await fetch('/api/recipe');
+          
+          const datas = await response.json();
+
+          // console.log(datas);
+          setFetchedDatas(datas);
+
+        }
+        catch(error) {
+          console.error(error)
+        }
+      }
+
+  useEffect(() =>{
+
+   
+    getDatas();
+
+  },[])
+
+
+  return(
+    <>
+      <PageWrapper>
+
+        <h1>Testing Fetching API</h1>
+        
+        <div className="debeug">
+          allo
+          
+          {fetchedDatas && fetchedDatas.map((recipe: Recipe) => (
+              <RecipeCard key={recipe.id} recipe={recipe} />
+            ))
+          }
+
+        </div>
+
+      </PageWrapper>
+
+    <StoreDebbuger/>
+    </>
+  )
+}
+export default SandBoxPage
