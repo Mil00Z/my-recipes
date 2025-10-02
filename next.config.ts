7a1f6090ad07550be2ea65037c580fc039439526
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-10-02 15:54:27 +0200

    Issue : nextJS HMR down , found hack to force the watcher

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  watchOptions: {
+    pollIntervalMs: 500, // Check for changes every second
+  },
   reactStrictMode: true, // Active une vérification plus stricte (optionnel)
   compiler: {
     styledComponents: true, // Peut corriger certains problèmes liés aux entités dans les styles
