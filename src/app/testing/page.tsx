7b3d0ecff44a5734a02e8f138d077eb6d0b20545
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-10-08 12:40:57 +0200

    testing : get another datas with call api

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-// @refresh reset
-
 import {useState,useEffect} from 'react';
 import RecipeCard from '@/components/RecipeCard/RecipeCard';
 
 
 import type { Recipe } from '@/types/recipe.types';
+import type { Ingredient } from '@/types/ingredient.types';
+
+//UI
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
 import StoreDebbuger from "@/components/Debeug/Debeug";
 
+
+//Styles
 import './Testing.scss'
 import '@/components/RecipeCard/RecipeCard.scss';
 
@@ -18,6 +21,24 @@ const SandBoxPage = () => {
 
   const [fetchedDatas,setFetchedDatas] = useState<Recipe[]>([])
 
+  const [fetchedIngredients,setFetchedIngredients] = useState<Ingredient>({})
+
+
+  const getIngredients = async () => {
+
+    try {
+      const response = await fetch('/api/ingredient');
+      const datas = await response.json();
+
+      setFetchedIngredients(datas);
+
+    }
+    catch(error){
+
+      console.error('erreure de call vers Ingredients',error);
+    }
+
+  }
 
   const getDatas = async () => {
 
@@ -41,10 +62,12 @@ const SandBoxPage = () => {
 
   
     getDatas();
+    // getIngredients();
 
   },[])
 
   
+
   return(
     <>
       <PageWrapper>
@@ -60,8 +83,17 @@ const SandBoxPage = () => {
             ))
           }
 
-
         </div>
+
+        {/* <div className="debeug testing">
+
+          { fetchedIngredients && fetchedIngredients.map((ingredient:Ingredient) => {
+
+
+              return (<span key={ingredient.id}>{ingredient.ingredient}</span>)
+
+          })}
+        </div> */}
 
       </PageWrapper>
 
