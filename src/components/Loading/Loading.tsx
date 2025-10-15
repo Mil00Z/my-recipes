b82dfddf523d47a50879277ad21a1d4e61c6f147
benJ <26485097+Mil00Z@users.noreply.github.com>
2025-10-15 15:58:38 +0200

    Loading : updat Loader + Conditional basic render

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,11 +1,20 @@
+import { useStore } from '@/hooks/dataStore';
+
+//Styles
+import './Loading.scss';
+
 const Loading = () => {
 
+  const {recipes,isLoading} = useStore();
 
-return (
-  <>
-    <p className="debeug">Loading datas...</p>
-  </>
-)
+  console.log('start','is loading =>',isLoading);
+
+  return (
+    <>
+      <h3>Loading datas...</h3>
+      <div className="loader"></div>
+    </>
+  )
 
 }
 export default Loading;
