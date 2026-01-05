99aa4d12d43486066f5e5a458969874a6dbb42b7
Mil00Z <Mil00Z@users.noreply.github.com>
2026-01-05 14:48:24 +0100

    Componant : update PageWrapper Props Layout with new entry

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header/Header";
 
 type PageWrapperProps = {
   children: React.ReactNode;
-  layout?: 'home' | undefined;
+<q></q>  layout?: 'home' | 'create' | 'update' | undefined;
 };
 
 const PageWrapper = ({children,layout = undefined}:PageWrapperProps) =>{
