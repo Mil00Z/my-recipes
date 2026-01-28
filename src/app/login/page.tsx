de6a3f97115cbbfe23b08d42970f2676bbae8bc9
Mil00Z <Mil00Z@users.noreply.github.com>
2026-01-28 10:25:12 +0100

    Login :  client, routing, semi testing Form and log in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
+import { createClient } from "@/utils/supabase/client";
 import PageWrapper from "@/components/PageWrapper/PageWrapper";
 
 //Styles
@@ -10,31 +12,70 @@ import "./Login.scss";
 
 export default function LoginPage() {
 
-    const [inputUserEmail, setInputUserEmail] = useState('');
-    const [inputUserPass, setInputUserPass] = useState();
+    //Settings
+    const router = useRouter();
+    const supabase = createClient();
 
-    //      const getUserEmail = (value) => {
+    //States
+    const [inputUserEmail, setInputUserEmail] = useState<string>('');
+    const [inputUserPass, setInputUserPass] = useState<string>('');
+    const [errorEmail, setErrorEmail] = useState<boolean>(false);
+    // const [errorPass, setErrorPass] = useState<boolean>(false);
 
-    //     let emailRegex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
+    const getUserEmail = (value: string) => {
 
-    //     if (value.length > 6 && emailRegex.test(value)) {
+        const emailRegex = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
 
-    //       setInputUserEmail(value);
+        if (value.length >= 6 && emailRegex.test(value)) {
 
-    //     }
+            setErrorEmail(false);
+            setInputUserEmail(value);
 
-    //   }
+        } else {
 
-    //   const getUserPass = (value) => {
+            setErrorEmail(true);
+        }
 
-    //     let regExTest = new RegExp("^[a-zA-Z0-9_-]+$");
+    }
 
-    //     if(value.length > 6 && regExTest.test(value)) {
+    const getUserPass = (value: string) => {
 
-    //       setInputUserPass(value);
+        const regExTest = new RegExp("^[a-zA-Z0-9_-]+$");
 
-    //     } 
+        if (value.length > 6 && regExTest.test(value)) {
 
+            setInputUserPass(value);
+
+        }
+
+    }
+
+
+
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+
+        const { error } = await supabase.auth.signInWithPassword({
+            email: inputUserEmail,
+            password: inputUserPass
+        })
+
+        if (error) {
+            console.log(error)
+            alert(error.message)
+            return;
+        } else {
+            console.log('success')
+            setTimeout(() => {
+
+                router.push('/recipes/50');
+
+            }, 3000)
+        }
+
+    }
 
 
     return (
@@ -43,15 +84,16 @@ export default function LoginPage() {
             <section className="login-form">
 
                 <h1><i className="fa fa-user-circle sign-in-icon"></i> LogIn</h1>
-                <form id="login">
+                <form id="login" onSubmit={(e) => handleLogin(e)}>
 
                     <div className="input-wrapper">
                         <label htmlFor="email">Username</label>
-                        <input type="email" placeholder="Votre identifiant" id="email" name="email" />
+                        <input type="email" placeholder="Votre identifiant" id="email" name="email" onChange={(e) => getUserEmail(e.target.value)} />
                     </div>
+                    {errorEmail && <p className="error">Veuillez remplir le champ email</p>}
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="*********" />
+                        <input type="password" id="password" name="password" placeholder="*********" onChange={(e) => getUserPass(e.target.value)} />
                     </div>
 
                     <button className="btn sign-in-button" type="submit">Go Auth</button>
