98d01847dad274159e06d32ec9db2f37b0b80420
Mil00Z <Mil00Z@users.noreply.github.com>
2026-03-05 18:03:32 +0100

    Recipe Page : props for Modale trigger Delete Recipe

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -337,7 +337,7 @@ const RecipeSingle = () => {
           </footer>
         </article>
 
-        <Modale title={fetchedRecipe.title} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} />
+        <Modale title={fetchedRecipe.title} modalIsOpen={modalIsOpen} setModalIsOpen={setModalIsOpen} onConfirm={handleDeleteRecipe} />
 
       </PageWrapper >
     </>
