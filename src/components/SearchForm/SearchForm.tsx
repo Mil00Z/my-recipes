6f84e691043ceb2f132d94f38de6b94f041d71ce
Mil00Z <Mil00Z@users.noreply.github.com>
2026-03-11 17:05:59 +0100

    Search : disable natif behavior + quick adjust

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -7,66 +7,65 @@ import type { Recipe } from "@/types/recipe.types";
 import "./SearchForm.scss";
 
 // interface SearchFormProps {
-  //   recipes: Recipe[];
+//   recipes: Recipe[];
 // } 
 
 const SearchForm = () => {
 
-  const {recipes,updateResults,tags} = useStore();
+  const { recipes, updateResults, tags } = useStore();
 
   //Constant Rules
   const minimumQueryLength = 3;
 
 
   //Checking Datas
-  function MainSearch(element:string) {
+  function MainSearch(element: string) {
 
     let baseSource = recipes;
 
     // complete feature search tags + Main Search
     if (tags.length > 0) {
-  
+
       baseSource = filterRecipesByTags(recipes, tags);
-  
+
     }
 
     if (element.length < minimumQueryLength) {
-          updateResults(baseSource); 
-          return; 
+      updateResults(baseSource);
+      return;
     }
 
 
     //Search
     const searchValue = element.toLowerCase();
 
-    const results = baseSource.filter((recipe:Recipe)=> {
+    const results = baseSource.filter((recipe: Recipe) => {
 
-                  const nameMatch = recipe.title.toLowerCase().includes(searchValue);
-                  const descriptionMatch = recipe.description.toLowerCase().includes(searchValue);
+      const nameMatch = recipe.title.toLowerCase().includes(searchValue);
+      const descriptionMatch = recipe.description.toLowerCase().includes(searchValue);
 
-                  const ingredientsArray = recipe.ingredients.map((ingredient)=> ingredient.ingredient.toLowerCase())
-                
-                  const ingredientMatch = ingredientsArray.some((ingredient)=> {
+      const ingredientsArray = recipe.ingredients.map((ingredient) => ingredient.ingredient.toLowerCase())
 
-                      return ingredient.includes(searchValue);
+      const ingredientMatch = ingredientsArray.some((ingredient) => {
 
-                  });
+        return ingredient.includes(searchValue);
 
-                  return nameMatch || descriptionMatch || ingredientMatch;
+      });
 
-              });
+      return nameMatch || descriptionMatch || ingredientMatch;
 
-      updateResults(results);
-    
-    
+    });
+
+    updateResults(results);
+
+
   }
+  return (
 
-return(
-
-    <form action="/" className="the-form">
-        <label htmlFor="main-search" className="v-hidden">Recherche</label>
-        <input type="search" name="main-search" id="main-search" onChange={(event) => MainSearch(event.target.value)} placeholder="Rechercher une recette, un ingrédient..." aria-label="barre de recherche de recettes" minLength={minimumQueryLength}/>
-        <button type="submit" className="btn btn-search" aria-label="bouton de recherche"><i className="fa-solid fa-search"></i></button>
+    <form className="the-form" onSubmit={(e) => e.preventDefault()}>
+      <label htmlFor="main-search" className="v-hidden">Recherche</label>
+      <input type="search" name="main-search" id="main-search" onChange={(event) => MainSearch(event.target.value)} placeholder="Rechercher une recette, un ingrédient..." aria-label="barre de recherche de recettes" minLength={minimumQueryLength} />
+      <button type="button" disabled className="btn btn-search" aria-label="icône de recherche"><i className="fa-solid fa-search"></i></button>
     </form>
   )
 }
